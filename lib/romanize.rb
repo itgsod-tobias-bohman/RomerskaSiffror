3f6fa2d170fe8c4f643e0e1554e1eafc5da4ee04
itgsod-tobias-bohman <tobias.bohman@elev.it-gymnasiet.se>
2014-05-22 11:04:46 +0200

    added tomdoc docs

--- lib/romanize.rb
+++ lib/romanize.rb
@@ -1,4 +1,4 @@
-# Public: Convert slöfjslödfjsdlöfj some text an arbitrary number of times.
+# Public: Convert an Integer into an romanized string.
 #
 # number  - The Integer to be duplicated.
 #
@@ -8,12 +8,19 @@
 #   # => 'IV'
 #
 # Returns the number converted to a romanized String.
+# Raises error if number = 0
+# Raises error if number < 0
 def romanize(number)
   raise ArgumentError, 'can not encode zero' if number == 0
   raise ArgumentError, 'can not encode negative number' if number < 0
 
+  # Internal: Integer to handle the input number
   input_number = number
+
+  # Internal: String to handle the roman output
   roman = ""
+
+  # Internal: Hash to handle to roman numbers next to Integers
   roman_numbers = {
       1000 => "M",
       900 => "CM",
@@ -29,8 +36,15 @@ def romanize(number)
       4 => "IV",
       1 => "I",
   }
+
+
   roman_numbers.each do |value, letter|
+
+    # Internal: Inserts current roman letter x times.
     roman << letter*(input_number / value)
+
+    # Internal: Decreases the input_number via modulo value.
+    #   Modulo divides input_number/value evenly, the rest of the numbers that can't be evenly divided gets pushed into input_number.
     input_number = input_number % value
   end
 
