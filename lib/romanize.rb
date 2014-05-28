47ffdbd074bb374a66130d14d9d1eb7d62d0c37a
itgsod-tobias-bohman <tobias.bohman@elev.it-gymnasiet.se>
2014-05-28 14:46:31 +0200

    removed unnecscscery stuffs

--- lib/romanize.rb
+++ lib/romanize.rb
@@ -14,13 +14,13 @@ def romanize(number)
   raise ArgumentError, 'can not encode zero' if number == 0
   raise ArgumentError, 'can not encode negative number' if number < 0
 
-  # Internal: Integer to handle the input number
+  # Integer to handle the input number
   input_number = number
 
-  # Internal: String to handle the roman output
+  # String to handle the roman output
   roman = ""
 
-  # Internal: Hash to handle to roman numbers next to Integers
+  # Hash to handle to roman numbers next to Integers
   roman_numbers = {
       1000 => "M",
       900 => "CM",
@@ -40,10 +40,10 @@ def romanize(number)
 
   roman_numbers.each do |value, letter|
 
-    # Internal: Inserts current roman letter x times.
+    # Inserts current roman letter x times.
     roman << letter*(input_number / value)
 
-    # Internal: Decreases the input_number via modulo value.
+    # Decreases the input_number via modulo value.
     #   Modulo divides input_number/value evenly, the rest of the numbers that can't be evenly divided gets pushed into input_number.
     input_number = input_number % value
   end
